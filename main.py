9d90017d0ea8a52d6e684f3d3fde5a106ec160d8
Mario Grabova <mgrabova@gmail.com>
2019-11-19 19:50:09 +0100

    ADDED main.py and requirements.txt

--- main.py
+++ main.py
@@ -4,5 +4,17 @@ description: Text classification with preprocessed text
 date: 19.11.2019
 """
 
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+
+from tensorflow import keras
+import tensorflow_datasets as tfds
+tfds.disable_progress_bar()
+
+import numpy as np
+
+
 if __name__ == '__main__':
     print('Text classification with preprocessed text')
+    print('Tensorflow: ', tf.__version__)
