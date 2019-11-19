b885989b4177d45583be287f5771a6edb677d5cd
Mario Grabova <mgrabova@gmail.com>
2019-11-19 19:17:48 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+author: Mario Grabovaj
+description: Text classification with preprocessed text
+date: 19.11.2019
+"""
+
+if __name__ == '__main__':
+    print('Text classification with preprocessed text')
